55ee5d4055196b31e40346a7b5421c53d8d190a6
Alan <afudge@usaviationacademy.com>
2021-06-18 18:42:01 -0500

    fix public path in output

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'build'),
         filename: 'bundle.js',
-        publicPath: "/"
+        publicPath: "./"
     },
     devtool: "inline-source-map",
     devServer: {
